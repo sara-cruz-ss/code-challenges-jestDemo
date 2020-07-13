34d69cbee0684a42456793934e1a954d8b93500a
Sara Cruz <sara.cruz@secret-source.eu>
2020-07-13 16:42:29 +0100

    challenge 1 2018 & new test cases

--- src/listen.ts
+++ src/listen.ts
@@ -1,8 +1,8 @@
-import { app } from './app';
+import { app } from "./app";
 
-const port = process.env.PORT || '8000';
+const port = process.env.PORT || "8000";
 
-app.listen(port, err => {
-    if (err) return console.error(err);
-    return console.log(`Server is listening on ${port}`);
-  });
+app.listen(port, (err) => {
+  if (err) return console.error(err);
+  return console.log(`Server is listening on ${port}`);
+});
--- test/first.spec.ts
+++ test/first.spec.ts
@@ -1,6 +1,8 @@
 import { app } from "../src/app";
 import { totalFuel } from "../src/app";
 import { pairs } from "../src/app";
+import { sum } from "../src/2018";
+import { splitStr } from "../src/2018";
 
 test("the app initializes", () => {
   expect(app.mountpath).toBe("/");
@@ -31,6 +33,30 @@ describe("Pairs function", () => {
       const expected = 3;
 
       const solution = pairs("1122");
+
+      expect(solution).toBe(expected);
+    });
+  });
+});
+
+describe("Sum function", () => {
+  describe("given a postive or negative number ", () => {
+    it("should increase or decrease the final result", () => {
+      const expected = 5;
+
+      const solution = sum("-1, +6");
+
+      expect(solution).toBe(expected);
+    });
+  });
+});
+
+describe("splitStr function", () => {
+  describe("given a text with positive and negative characters", () => {
+    it("should separate every integer from the next one without spaces", () => {
+      const expected = [-7, 15];
+
+      const solution = splitStr(`-7 +15`);
 
       expect(solution).toBe(expected);
     });
